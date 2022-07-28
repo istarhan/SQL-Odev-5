0ac3f400771a9184740fde575304fad7c55dd3f5
istarhan <51276149+istarhan@users.noreply.github.com>
2022-07-28 16:55:18 +0300

    Update Odev5.sql

--- Odev5.sql
+++ Odev5.sql
@@ -1,1 +1,6 @@
 --Soru 1
+SELECT title , length  FROM film WHERE title LIKE '%n'ORDER BY length DESC LIMIT 5 ;
+--Soru 2
+SELECT title , length  FROM film WHERE title LIKE '%n'ORDER BY length ASC OFFSET 5 LIMIT 5;
+--Soru 3
+SELECT last_name , store_id  FROM customer WHERE store_id = 1 ORDER BY  last_name DESC LIMIT 4;
